5f0377769be1e98154b3a594e43a7db9306af1e0
KenKwek <kennethkwek.2024@computing.smu.edu.sg>
2025-10-21 10:28:29 +0800

    Finished Ex 2

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -3,9 +3,21 @@
         // Add code in appropriate places
         data() {
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                newItem: ''
             }
             
+        },
+        methods: {
+            deleteItem(index) {
+                this.items.splice(index, 1);
+            },
+            addItem() {
+                if (this.newItem.trim() !== '') {
+                    this.items.push(this.newItem.trim());
+                    this.newItem = '';
+                }
+            }
         }
     }
 </script>
@@ -14,5 +26,13 @@
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
-   
+    <ul>
+        <li v-for="(item, index) in items">
+            {{ item }} 
+            <button @click="deleteItem(index)">Delete!</button>
+        </li>
+    </ul>
+    
+    <input type="text" v-model="newItem"> 
+    <button @click="addItem">Add!</button>
 </template>
